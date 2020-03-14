3660f69ec686e5fb5c9d8714146a92c4b9286ef7
mobad <mostafa.m.badawey@gmail.com>
2020-03-14 22:59:01 +0200

    create sign in component

---
+++ src/components/auth-page/Signin.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Signin = () => {
+    return (
+        <div>
+            <input id='email' placeholder='Email' type='text'/>
+            <input id='pass' placeholder='Password' type='password'/>
+        </div>
+    );
+};
+
+export default Signin;
