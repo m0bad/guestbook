c3e021926bacbe019837783b4f4f368a4469cba2
mobad <mostafa.m.badawey@gmail.com>
2020-03-14 22:58:44 +0200

    create signup component

---
+++ src/components/auth-page/Signup.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+export const Signup = () => {
+    return (
+        <div>
+            <input id='email' placeholder='Email' type='text'/>
+            <input id='pass' placeholder='Password' type='password'/>
+            <input id='repass' placeholder='Repeat password' type='password'/>
+        </div>
+);
+            };
+
+export default Signup;
