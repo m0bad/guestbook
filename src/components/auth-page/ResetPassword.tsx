89d674b2cd41c63b57dfd41b62ff766ca7e2edc1
mobad <mostafa.m.badawey@gmail.com>
2020-03-14 22:59:56 +0200

    create reset password component

---
+++ src/components/auth-page/ResetPassword.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResetPassword = () => {
+    return (
+        <div>
+            <input id='email' placeholder='Email' type='text'/>
+        </div>
+    );
+};
+
+export default ResetPassword;
