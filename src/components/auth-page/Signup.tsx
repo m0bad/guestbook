209481fdda7c5b74d6a75484ffbfdab07dbe946e
mobad <mostafa.m.badawey@gmail.com>
2020-03-15 15:43:11 +0200

    edit signup inputs values to be dynamic

--- src/components/auth-page/Signup.tsx
+++ src/components/auth-page/Signup.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-export const Signup = () => {
+// eslint-disable-next-line no-unused-vars
+import {UserDataType} from "../../types/UserDataType";
+
+type SignUpProps = {
+    handleChange(): void;
+    value: UserDataType
+}
+const Signup: React.FC<SignUpProps> = ({value, handleChange}: SignUpProps) => {
     return (
         <div>
-            <input id='email' placeholder='Email' type='text'/>
-            <input id='pass' placeholder='Password' type='password'/>
-            <input id='repass' placeholder='Repeat password' type='password'/>
+            <input
+                id='email'
+                placeholder='Email'
+                type='text' name='email'
+                value={value.email}
+                onChange={handleChange}
+            />
+            <input
+                id='pass'
+                placeholder='Password'
+                type='password'
+                name='password'
+                value={value.password}
+                onChange={handleChange}
+            />
+            <input
+                id='repass'
+                placeholder='Repeat password'
+                type='password'
+                name='rePassword'
+                value={value.rePassword}
+                onChange={handleChange}/>
         </div>
-);
-            };
+    );
+};
 
 export default Signup;
