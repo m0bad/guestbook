702dc9e02d86ceab117d19f6d9a8882c777c2a0d
mobad <mostafa.m.badawey@gmail.com>
2020-03-15 16:12:57 +0200

    add login service

--- src/services/authService.tsx
+++ src/services/authService.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import {UserDataType} from "../types/UserTypes";
+import {LoginDataType, UserDataType} from "../types/UserTypes";
 
 export const registerService = async (data: UserDataType) => {
     const {email, password, rePassword} = data;
@@ -22,4 +22,26 @@ export const registerService = async (data: UserDataType) => {
         throw new Error('ERROR at register')
 
     }
+};
+
+
+export const loginService = async (data: LoginDataType) => {
+    const {email, password} = data;
+    const result = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email, password})
+    });
+
+    if (result.status === 200) {
+        return result.json();
+    } else {
+        // TODO: handle this later
+        throw new Error('ERROR at login')
+
+    }
+
 };
