098df9f8d191c0bf6acc146fc3ded355cad70a53
mobad <mostafa.m.badawey@gmail.com>
2020-03-15 15:37:50 +0200

    wrap app with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Switch, Route} from "react-router-dom";
 import LandingPage from "./components/landing-page/LandingPage";
+import AuthPage from "./components/auth-page/AuthPage";
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
     return (
         <div>
-            <LandingPage />
+            <AuthContextProvider>
+                <Switch>
+                <Route exact path='/' component={LandingPage}/>
+                <Route exact path='/auth' component={AuthPage}/>
+            </Switch>
+            </AuthContextProvider>
         </div>
     );
 }
