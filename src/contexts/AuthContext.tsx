7f74d10eb83f93d6a4bfc79022d7368522a512e2
mobad <mostafa.m.badawey@gmail.com>
2020-03-15 15:37:19 +0200

    create user authentication context

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,33 @@
+import React, {createContext, useState} from 'react';
+import {registerService}  from "../services/authService";
+// eslint-disable-next-line no-unused-vars
+import {UserDataType} from "../types/UserDataType";
+
+// @ts-ignore
+export const AuthContext = createContext();
+
+const AuthContextProvider = (props: any) => {
+    const [authState, setAuthState] = useState({
+        // TODO: is LoggedIn should by dynamic
+        isLoggedIn: false,
+        user: {},
+        email: '',
+        password: '',
+        rePassword: '',
+        error: '',
+    });
+
+    const register = async (data: UserDataType) => {
+        const registerUser = await registerService(data);
+        setAuthState({...authState, user: registerUser});
+    };
+
+    return (
+        <AuthContext.Provider value={{authState, register}}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+
+};
+
+export default AuthContextProvider;
