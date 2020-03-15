3910f7820d21f0ada77d79e78abe37127e632c41
mobad <mostafa.m.badawey@gmail.com>
2020-03-15 16:20:23 +0200

    refactor signin component to use state

--- src/components/auth-page/Signin.tsx
+++ src/components/auth-page/Signin.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import {LoginDataType} from "../../types/UserTypes";
 
-const Signin = () => {
+type SignInProps = {
+    handleChange(): void;
+    value: LoginDataType
+}
+const Signin: React.FC<SignInProps> = ({value, handleChange}: SignInProps) => {
     return (
         <div>
-            <input id='email' placeholder='Email' type='text'/>
-            <input id='pass' placeholder='Password' type='password'/>
+            <input
+                id='email'
+                placeholder='Email'
+                name="email"
+                type='text'
+                value={value.email}
+                onChange={handleChange}
+            />
+            <input
+                id='pass'
+                name="password"
+                placeholder='Password'
+                type='password'
+                value={value.password}
+                onChange={handleChange}
+            />
         </div>
     );
 };
