fbd58b7d8089a294810d06cac173b2479629ff3e
mobad <mostafa.m.badawey@gmail.com>
2020-03-15 16:19:04 +0200

    refactor auth service by extract the api logic out of services

--- src/services/authService.tsx
+++ src/services/authService.tsx
@@ -1,19 +1,23 @@
 // eslint-disable-next-line no-unused-vars
 import {LoginDataType, UserDataType} from "../types/UserTypes";
 
+const apiHelper = async (reqType: string, url: string, body: any): Promise<any> => {
+    return fetch(url, {
+        method: reqType,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+};
+
 export const registerService = async (data: UserDataType) => {
     const {email, password, rePassword} = data;
     if (password !== rePassword) {
         throw new Error('invalid email/password')
     }
-    const result = await fetch('/api/auth/signup', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({email, password})
-    });
+    const result = await apiHelper('POST', '/api/auth/signup', {email, password});
 
     if (result.status === 200) {
         return result.json();
@@ -27,15 +31,7 @@ export const registerService = async (data: UserDataType) => {
 
 export const loginService = async (data: LoginDataType) => {
     const {email, password} = data;
-    const result = await fetch('/api/auth/signin', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({email, password})
-    });
-
+    const result = await apiHelper('POST', '/api/auth/signin', {email, password});
     if (result.status === 200) {
         return result.json();
     } else {
