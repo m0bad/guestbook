931f72fcfd7caa5dd954a2ec4a7ec0bb135643b5
mobad <mostafa.m.badawey@gmail.com>
2020-03-16 16:02:49 +0200

    move message list location component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Switch, Route} from "react-router-dom";
 import LandingPage from "./components/landing-page/LandingPage";
 import AuthPage from "./components/auth-page/AuthPage";
-import MessagesList from "./components/messages-page/MessagesList";
+import MessagesList from "./components/messages-page/messages-list/MessagesList";
 import AuthContextProvider from "./contexts/AuthContext";
 import PrivateRoute from "./routes/PrivateRoutes";
 
