f9deed25d844cea571c93b28c5da531416f0f7e0
mobad <mostafa.m.badawey@gmail.com>
2020-03-16 16:30:49 +0200

    change complain to be number of replies

--- src/components/messages-page/bottom-message/BottomMessage.tsx
+++ src/components/messages-page/bottom-message/BottomMessage.tsx
@@ -9,7 +9,7 @@ export const BottomMessage = (props: Props) => {
         <div className="bottom-message">
             <div className="bottom-message-date">Aug 23, 2014 @ 10:32 AM</div>
             <ul className="bottom-message-actions">
-                <li className="bottom-message-n0-replies">Complain</li>
+                <i className="bottom-message-n0-replies">100 reply</i>
                 <li className="bottom-message-reply">Reply</li>
             </ul>
         </div>
