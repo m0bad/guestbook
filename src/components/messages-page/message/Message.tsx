dfcb65bf6a4f9b8381b9c86be2f09e03ca2ba2f3
mobad <mostafa.m.badawey@gmail.com>
2020-03-19 17:40:28 +0200

    use props in Message component

--- src/components/messages-page/message/Message.tsx
+++ src/components/messages-page/message/Message.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import './message-style.css';
 import {MessageUserInfo} from "../message-user-info/MessageUserInfo";
 import {MessageContent} from "../message-content/MessageContent";
+// eslint-disable-next-line no-unused-vars
+import {MessageDataType} from "../../../types/MessageTypes";
 
-type Props = {};
-export const Message = (props: Props) => {
-    console.log(props);
+type MessageProps = {
+    messageData: MessageDataType
+};
+export const Message: React.FC<MessageProps> = (props: MessageProps) => {
+    const {text, replies, createdAt} = props.messageData;
     return (
-         <div className="message-wrap">
-             <MessageUserInfo />
-             <MessageContent />
-         </div>
+        <div className="message-wrap">
+            <MessageUserInfo/>
+            <MessageContent
+                text={text}
+                numOfReplies={replies?.length || 0}
+                createdAt={createdAt}
+            />
+        </div>
     );
 };
