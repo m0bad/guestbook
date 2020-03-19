04fc156d63ac1fac18794a31fa8d81e4fc43b594
mobad <mostafa.m.badawey@gmail.com>
2020-03-19 17:40:05 +0200

    use messages from context in messagesList

--- src/components/messages-page/messages-list/MessagesList.tsx
+++ src/components/messages-page/messages-list/MessagesList.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './messagesList-style.css';
 import {Message} from "../message/Message";
 import {NewMessageForm} from "../new-message-form/NewMessageForm";
+import {MessageContext} from "../../../contexts/MessageContext";
+// eslint-disable-next-line no-unused-vars
+import {MessageDataType} from "../../../types/MessageTypes";
 
-type MessagesListProps = {
-
-};
-const MessagesList: React.FC<MessagesListProps> = () => {
+const MessagesList: React.FC<{}> = () => {
+    const {messages} = useContext(MessageContext);
     return (
         <div>
             <div className="message-list-container">
-                <Message/>
-                <Message/>
-                <Message/>
-                <NewMessageForm />
+                {
+                    messages
+                        ? messages.map((message: MessageDataType) => <Message key={message._id} messageData={message}/>)
+                        : <p>loading...</p>
+                }
+                <NewMessageForm/>
             </div>
         </div>
     );
