40b9f2d111b2a59a42cf536cf1ef1514c7600b67
mobad <mostafa.m.badawey@gmail.com>
2020-03-19 21:21:08 +0200

    create new Message Functionality

--- src/components/messages-page/new-message-form/NewMessageForm.tsx
+++ src/components/messages-page/new-message-form/NewMessageForm.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import './nes-message-form.css';
 import {MessageUserInfo} from "../message-user-info/MessageUserInfo";
+import {MessageContext} from "../../../contexts/MessageContext";
+import {AuthContext} from "../../../contexts/AuthContext";
+// eslint-disable-next-line no-unused-vars
+import {NewMessageData} from "../../../types/MessageTypes";
 
-type Props = {};
-export const NewMessageForm = (props: Props) => {
+export const NewMessageForm: React.FC<{}> = () => {
+    const {addMessage} = useContext(MessageContext);
+    const {authState} = useContext(AuthContext);
+    const [newMessage, setNewMessage] = useState('');
+
+
+    const submitMessage = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (event.key === 'Enter') {
+            let data: NewMessageData = {
+                text: newMessage,
+                author: authState.user._id
+            };
+            // @ts-ignore
+            addMessage(data).then(() => setNewMessage(''));
+        }
+    };
+    const onKeyChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        let target = event.target;
+        setNewMessage(target.value);
+    };
     return (
         <div className="new-message-container">
             <MessageUserInfo/>
             <div className="new-message-block">
-                    <textarea name={''} id="" cols={30} rows={3} placeholder="Add comment...">{null}</textarea>
+                <textarea
+                    cols={30}
+                    rows={3}
+                    placeholder="Add comment..."
+                    onChange={onKeyChange}
+                    onKeyDown={submitMessage}
+                    value={newMessage}
+                >
+                </textarea>
             </div>
         </div>
     );
